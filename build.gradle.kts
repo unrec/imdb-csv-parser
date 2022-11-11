4255f9454208efce034b3e102368ba8faa1924a6
unrec <andrei.troitskiy@gmail.com>
2022-11-11 15:54:07 +0100

    detekt support added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 version = "0.0.1-SNAPSHOT"
 group = "com.unrec"
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -5,6 +7,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 plugins {
     kotlin("jvm") version "1.6.21"
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("io.gitlab.arturbosch.detekt").version("1.21.0")
 }
 
 dependencies {
@@ -12,6 +15,25 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    config = files("$projectDir/detekt-config.yml")
+    parallel = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "1.8"
+
+    reports {
+        txt.required.set(false)
+        html.required.set(true)
+        with(xml) {
+            required.set(true)
+            outputLocation.set(file("$buildDir/reports/detekt/detekt.xml"))
+        }
+    }
 }
 
 repositories {
